ac2759e139e1610825c00a108af8ef9b9b2ca2fc
Jensert <47450434+Jensert@users.noreply.github.com>
2024-12-12 12:18:16 +0100

    Add files via upload

--- main.lua
+++ main.lua
@@ -11,14 +11,23 @@ local Game = {
     player = nil,
     ---@type map
     map = nil,
+
+    window_width = nil,
+    window_height = nil,
+    buffer = nil,
+    renderTarget = nil
 }
 
 function Game:load()
+    love.window.setTitle("Snake")
+    self.window_width = love.graphics.getWidth()
+    self.window_height = love.graphics.getHeight()
+    self.renderTarget = love.graphics.newCanvas(self.window_width, self.window_height)
     self.player = snake:new()
     self.map = map:new(20, 20, 20)
 end
 
-function Game:update()
+function Game:update(dt)
     self.player:update()
     if(collisionManager:collisionBetweenSnakeAndFood(self.player, food)) then
         gamemanager.score = gamemanager.score + 1
@@ -28,19 +37,21 @@ function Game:update()
 end
 
 function Game:draw()
+    
     self.map:draw()
     self.player:draw(self.map)
     food:draw(self.map)
     love.graphics.setColor(1,1,1)
     love.graphics.print("Score: " .. tostring(gamemanager.score), 10, 450, 0, 2, 2, 0, 0, 0)
+
 end
 
 function love.load()
     Game:load()
 end
 
-function love.update()
-    Game:update()
+function love.update(dt)
+    Game:update(dt)
 end
 
 function love.draw()
